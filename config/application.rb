9299405e3839e210fd3473b790e27200ca8590a5
Carol Nichols <carol.nichols@gmail.com>
2014-06-01 12:31:47 -0400

    Update bundler load
    As recommended by regenerating rails config

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,12 @@ require 'radius'
 require 'trusty_cms/extension_loader'
 require 'trusty_cms/initializer'
 
-
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module TrustyCms
 class Application < Rails::Application
