b084eb6c2dbc9f19e6a46cbc436031c7167962cf
Carol Nichols <carol.nichols@gmail.com>
2014-06-14 19:08:48 -0400

    Update the layout based on rails 3 recommendations/features
    Make a manifest for the js instead of requiring all the files in the
    layout. Add csrf token meta tags. Some other little tweaks.

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into admin.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require 'admin/prototype'
+//= require 'admin/effects'
+//= require 'admin/dragdrop'
+//= require 'admin/lowpro'
+//= require 'admin/dateinput'
+//= require 'admin/pagestatus'
+//= require 'admin/cookie'
+//= require 'admin/popup'
+//= require 'admin/pagefield'
+//= require 'admin/dropdown'
+//= require 'admin/status'
+//= require 'admin/utility'
+//= require 'admin/codearea'
+//= require 'admin/tabcontrol'
+//= require 'admin/ruledtable'
+//= require 'admin/sitemap'
+//= require 'admin/shortcuts'
+//= require 'admin/toggle'
+//= require 'admin/validationerror'
+//= require 'admin/application'
