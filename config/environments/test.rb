e835bdf91483908c396ea2ca536b836d9bc25acc
Carol Nichols <carol.nichols@gmail.com>
2014-06-21 17:01:50 -0400

    Add a setting in dev and test to make mass assignment errors obvious
    As recommended by
    http://code.tutsplus.com/tutorials/mass-assignment-rails-and-you--net-31695,
    set this in dev and test so that you get explicit mass assignment errors
    in dev and test so that it's clearer what's going on and what needs to
    be fixed.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,6 +23,10 @@ TrustyCms::Application.configure do
   config.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
 
+  # Raise an ActiveModel::MassAssignmentSecurity::Error any time
+  # something is mass-assigned that shouldn't be for ease in debugging.
+  config.active_record.mass_assignment_sanitizer = :strict
+
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
