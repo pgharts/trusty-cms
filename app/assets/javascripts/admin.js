4add60cdbc8d894369fd6b378226259d8e4d4151
Carol Nichols <carol.nichols@gmail.com>
2014-06-21 17:55:41 -0400

    Remove prototype

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,7 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require 'admin/prototype'
 //= require 'admin/effects'
 //= require 'admin/dragdrop'
 //= require 'admin/lowpro'
