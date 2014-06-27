cfd4ec659763ee509872db63d14b9ccd4b1bfec5
Alicia L. Ferguson <alicialferguson@gmail.com>
2014-06-27 10:21:51 -0400

    Adding layout tests. Issue #95.

--- spec/features/layouts_spec.rb
+++ spec/features/layouts_spec.rb
@@ -15,8 +15,37 @@ describe 'Layouts (Design)' do
     end
 
     it 'lets you add a layout' do
-      skip 'Issue #58'
       click_link 'New Layout'
+      fill_in 'Name', with: 'Petunias'
+      fill_in 'Body', with: 'Wisteria'
+      click_button 'Create Layout'
+    end
+  end
+
+  context 'with a layout' do
+    before(:each) do
+      Layout.create!(name: 'Petunias', body: 'Wisteria')
+      visit '/admin/layouts'
+    end
+
+    it 'lets you edit the homepage' do
+      click_link 'Petunias'
+
+      expect(page).to have_content 'Edit Page'
+      expect(page).to have_field 'Page Title', with: 'Voyager Home'
+      expect(page).to have_button 'Save Changes'
+      expect(page).to have_content 'Last Updated by Kathryn Janeway'
+    end
+
+    it 'lets you remove the homepage' do
+      click_link 'Remove'
+
+      expect(page).to have_content 'Are you sure you want to permanently remove the following Page?'
+
+      click_button 'Delete Page'
+
+      expect(page).to have_content 'No Pages'
+      expect(page).to have_link 'New Homepage'
     end
   end
 end
