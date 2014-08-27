e8d57bdfbcea3764d5cfa9ec82f8c7a4b6873f3a
Alicia L. Ferguson <alicialferguson@gmail.com>
2014-08-27 10:23:02 -0400

    Issue #113. Getting the available tags link to make an ajax call to display tags. It is currently not working the way it is intended to (the link works and makes the ajax call and changes the DOM but it is not popping up properly.) The jQuery filter part was added but it has not been configured to work yet. Popup first!

--- app/assets/javascripts/admin/tags.js
+++ app/assets/javascripts/admin/tags.js
@@ -8,7 +8,32 @@ $(function(){
       dataType: "html",
       success: function(url){
           $('#popups').append(url);
+          $('#tag_reference_popup');
       }
     });
+  });
+
+  $("#filter").keyup(function(){
+
+    // Retrieve the input field text and reset the count to zero
+    var filter = $(this).val(), count = 0;
+
+    // Loop through the tag list
+    $(".commentlist li").each(function(){
+
+      // If the list item does not contain the text phrase fade it out
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+        $(this).fadeOut();
+
+        // Show the list item if the phrase matches and increase the count by 1
+      } else {
+        $(this).show();
+        count++;
+      }
+    });
+
+    // Update the count
+    var numberItems = count;
+    $("#filter-count").text("Number of Comments = "+count);
   });
 });
