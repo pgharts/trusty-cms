c192f8b8abccbd3ddbd86cd4cb1f3ca8072b06a4
Alicia L. Ferguson <alicialferguson@gmail.com>
2014-09-02 11:56:32 -0700

    Fixed jQuery code to be properly nested. Filter now works to properly filter searched for tags. Resolves issue #113.

--- app/assets/javascripts/admin/tags.js
+++ app/assets/javascripts/admin/tags.js
@@ -9,35 +9,33 @@ $(function(){
       success: function(data, textStatus, xhr){
         $('#popups').append(data);
         Popup.show('tag_reference_popup');
+        $('#filter').keyup(function(){
+
+          // Retrieve the input field text and reset the count to zero
+          var filter = $(this).val(), count = 0;
+
+          // Loop through the tag list
+          $(".tag_description").each(function(){
+
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+              $(this).fadeOut();
+
+              // Show the list item if the phrase matches and increase the count by 1
+            } else {
+              $(this).show();
+              count++;
+            }
+          });
+
+          // Update the count
+          $("#filter-count").text("Tags Found = "+count);
+        });
         $('.close_link').click(function(){
           Popup.close();
           $('#tag_reference_popup').remove();
         });
       }
     });
-  });
-
-  $("#filter").keyup(function(){
-
-    // Retrieve the input field text and reset the count to zero
-    var filter = $(this).val(), count = 0;
-
-    // Loop through the tag list
-    $(".tag_description").each(function(){
-
-      // If the list item does not contain the text phrase fade it out
-      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-        $(this).fadeOut();
-
-        // Show the list item if the phrase matches and increase the count by 1
-      } else {
-        $(this).show();
-        count++;
-      }
-    });
-
-    // Update the count
-    var numberItems = count;
-    $("#filter-count").text("Number of Comments = "+count);
   });
 });
