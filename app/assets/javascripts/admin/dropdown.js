47379b7961a7aac5e504030a450c85b3b23f8d93
Eric Sipple <saalon@gmail.com>
2014-09-08 08:25:10 -0400

    Fixing child page dropdowns.

--- app/assets/javascripts/admin/dropdown.js
+++ app/assets/javascripts/admin/dropdown.js
@@ -1,13 +1,15 @@
 (function(Dropdown, $) {
   Dropdown.setup = function(link) {
     var menuToWrap = Dropdown.findMenu(link);
-    menuToWrap.wrap("<div class='dropdown_wrapper' style='position: absolute; display: none'></div>");
+    if ($(menuToWrap).parent("div").length == 0) {
+      menuToWrap.wrap("<div class='dropdown_wrapper' style='position: absolute; display: none'></div>");
 
-    $(link).on('click', function(event){
-      event.preventDefault();
-      $(this).toggleClass('selected');
-      Dropdown.findMenu(this).closest('.dropdown_wrapper').slideToggle();
-    });
+      $(link).on('click', function (event) {
+        event.preventDefault();
+        $(this).toggleClass('selected');
+        Dropdown.findMenu(this).closest('.dropdown_wrapper').slideToggle();
+      })
+    };
   }
 
   Dropdown.findMenu = function(link) {
