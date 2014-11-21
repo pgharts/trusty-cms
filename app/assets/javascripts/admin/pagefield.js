4726cc56fecb4d99d3be10f2b3b605fc31b7f62c
Eric Sipple <saalon@gmail.com>
2014-11-21 14:28:34 -0500

    Add/remove field buttons for page fields now working. Issue #129

--- app/assets/javascripts/admin/pagefield.js
+++ app/assets/javascripts/admin/pagefield.js
@@ -1,54 +1,35 @@
-function addField(form) {
-  if (validFieldName()) {
-    new Ajax.Updater(
-      $('attributes').down('tbody'),
-      relative_url_root + '/admin/page_fields/',
-      {
-        asynchronous: true,
-        evalScripts: true,
-        insertion: 'bottom',
-        onComplete: function(response){ fieldAdded(form); },
-        onLoading: function(request){ fieldLoading(form); },
-        parameters: Form.serialize(form)
-      }
-    );
+PageFields = {
+  attachEvents: function() {
+
+    $('#add_page_field_button').click(function(e){
+      e.preventDefault();
+      $.ajax({
+        method: 'post',
+        url: '/admin/page_fields/',
+        data: $('#new_page_field').serialize(),
+        success: function(data, textStatus, jqXHR) {
+          PageFields.updateFields(data);
+        }
+      });
+    });
+
+    $('.remove_field').click(function(e){
+      e.preventDefault();
+      var row = $(this).closest('tr');
+      row.find(".delete_input").val(true);
+      row.find(".page_field_name").val('');
+      row.hide();
+    });
+
+  },
+  updateFields: function(element) {
+    $('.fieldset tr:last').after(element);
+    Popup.close();
+    $('#new_page_field').trigger('reset');
+    PageFields.attachEvents();
   }
-}
-function removeField(button) {
-  var row = $(button).up('tr');
-  var name = row.down('label').innerHTML;
-  if (confirm('Remove the "' + name + '" field?')) {
-    row.down('.delete_input').setValue(true);
-    row.down('.page_field_name').clear();
-    row.hide();
-  }
-}
-function fieldAdded(element) {
-  $(element).previous('.busy').hide();
-  $(element).down('.button').enable();
-  $(element).up('.popup').closePopup();
-  var field_index = $('page_field_counter').value;
-  $('page_fields_attributes_' + field_index + '_content').focus();
-  $('page_field_counter').setValue(Number(field_index).succ());
-  $('new_page_field').reset();
-}
-function fieldLoading(element) {
-  $(element).down('.button').disable();
-  $(element).previous('.busy').appear();
-}
-function validFieldName() {
-  var fieldName = $('page_field_name');
-  var name = fieldName.value.toLowerCase();
-  if (name.blank()) {
-    alert('Field name cannot be empty.');
-    return false;
-  }
-  if (findFieldByName(name)) {
-    alert('Field name must be unique.');
-    return false;
-  }
-  return true;
-}
-function findFieldByName(name) {
-  return $('attributes').select('input.page_field_name').detect(function(input) { return input.value.toLowerCase() == name; });
-}
+};
+
+$(function() {
+  PageFields.attachEvents();
+});
