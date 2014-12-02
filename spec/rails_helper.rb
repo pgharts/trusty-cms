987f21a12464e94f57771910fed35f68d0fc0f51
Eric Sipple <saalon@gmail.com>
2014-12-02 13:33:39 -0500

    Upping Capybara timeout.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, timeout: 30)
+  Capybara::Poltergeist::Driver.new(app, timeout: 60)
 end
 
 require 'database_cleaner'
