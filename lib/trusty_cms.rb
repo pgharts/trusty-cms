cb91da719539092d1c90ebeb4207977bd7defe3c
Eric Sipple <saalon@gmail.com>
2014-12-11 13:42:11 -0500

    Incrimenting gem to 1.1.1

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -5,7 +5,7 @@ unless defined? TrustyCms::Version
     module Version
       Major = '1'
       Minor = '1'
-      Tiny  = '0'
+      Tiny  = '1'
       Patch = nil # set to nil for normal release
 
       class << self
