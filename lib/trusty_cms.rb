48a1e248404587f7628b21a0f0f006db0f46c773
Eric Sipple <saalon@gmail.com>
2014-12-26 12:03:50 -0500

    1.1.7

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -5,7 +5,7 @@ unless defined? TrustyCms::Version
     module Version
       Major = '1'
       Minor = '1'
-      Tiny  = '7'
+      Tiny  = '8'
       Patch = nil # set to nil for normal release
 
       class << self
