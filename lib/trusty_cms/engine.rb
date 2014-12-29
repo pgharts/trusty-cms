bfe008faf3409838c9a09d5188a9efd451abbbda
Eric Sipple <saalon@gmail.com>
2014-12-29 14:39:12 -0500

    Adding ckeditor js to engine assets.

--- lib/trusty_cms/engine.rb
+++ lib/trusty_cms/engine.rb
@@ -2,7 +2,7 @@ module TrustyCms
   class Engine < Rails::Engine
     paths["app/helpers"] = []
     initializer "trusty_cms.assets.precompile" do |app|
-      app.config.assets.precompile += %w(admin/main.css admin.js)
+      app.config.assets.precompile += %w(admin/main.css admin.js admin/ckeditor/config.js admin/ckeditor/contents.css)
     end
   end
 end
