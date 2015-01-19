e65b6657a315b337dd0a3bac017b6ec3778ec93f
Eric Sipple <saalon@gmail.com>
2015-01-19 09:37:06 -0500

    Attempting to get styles.js to compile for ckeditor

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -5,7 +5,7 @@ unless defined? TrustyCms::Version
     module Version
       Major = '1'
       Minor = '1'
-      Tiny  = '22'
+      Tiny  = '23'
       Patch = nil # set to nil for normal release
 
       class << self
