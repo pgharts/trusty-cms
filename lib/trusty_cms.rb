69587d1ff855a02a8b33cf1afef4a514ff8bad2c
Eric Sipple <saalon@gmail.com>
2015-07-28 07:36:11 -0400

    2.0.5-beta

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = "2.0.4-beta"
+    VERSION = "2.0.5-beta"
   end
 end
