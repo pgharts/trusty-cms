beed4e2e14b0f7817bba0b3828d1aa72ffa31555
Brittany Martin <brittany.jill.martin@gmail.com>
2016-04-01 09:48:49 -0400

    Register version 2.0.20

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = "2.0.2"
+    VERSION = "2.0.20"
   end
 end
