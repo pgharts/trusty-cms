b1eddb344c236f9c199aff97e7d4c865a4337a92
Danielle Greaves <danigreaves@gmail.com>
2016-09-01 15:43:08 -0400

    BugzID: 3529 - Implements jQuery Sortable
    created a js file to sort the tables
    I added some configs to ensure the placement of the tr doesn't go above the folder it's in and cannot be moved above the absolute parent folder. The top level folder cannot be moved. An expanded folder cannot be moved.
    
    I also set up some bones in order to submit the position and save it. This will submit via ajax.

--- app/assets/javascripts/admin/sortable.js
+++ app/assets/javascripts/admin/sortable.js
@@ -9,11 +9,33 @@ $(document).ready(function() {
 
 
   $('#pages tbody').sortable({
+    beforeStop: function(event,ui) {
+      table_rows = ui.item.nextAll('tr');
+      for(i=0;i<table_rows.length;i++) {
+        var parent_id = table_rows.eq(i).attr('id');
+        if (parent_id != undefined) {
+          parent_id = parent_id.replace('page_','');
+          var this_id = ui.item.attr('data-tt-parent-id');
+          if(this_id == parent_id) {
+            $(this).sortable('cancel');
+          }
+        }
+      }
+    },
     helper: fixHelper,
     placeholder: "ui-state-highlight",
+    cancel: ".expanded",
     update: function(event, ui) {
-      var data = $(this).serialize();
-
+      table_rows = $(this).children();
+      var data = [];
+      for(i=0;i<table_rows.length;i++) {
+        var this_id = table_rows.eq(i).attr('id').replace("page_", "");
+        var position = i;
+        var parent_id = table_rows.eq(i).attr('data-tt-parent-id');
+        data.push(this_id, parent_id, position)
+      }
+      data.splice(0,3);
+      console.log(data);
       $.ajax({
         data: data,
         type: 'POST',
