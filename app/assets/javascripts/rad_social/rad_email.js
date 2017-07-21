6ee525831cc55d4a984a3cc597cb64a7b53c475b
Danielle Greaves <danigreaves@gmail.com>
2017-07-21 14:44:36 -0400

    BugzID: 4248 - Loading in rad_social javascripts and updated cc path

--- app/assets/javascripts/rad_social/rad_email.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require 'lib/rad_social/jquery.validate.min'
-//= require 'lib/rad_social/rad_ajax_form'
-//= require 'lib/rad_social/captcha'
-//= require 'lib/rad_social/rad_email_validator'
-//= require 'lib/rad_social/rad_email_form'
--- lib/trusty_cms/engine.rb
+++ lib/trusty_cms/engine.rb
@@ -5,7 +5,7 @@ module TrustyCms
       app.config.assets.precompile += %w(admin/main.css admin.js ckeditor/config.js
         ckeditor/contents.css ckeditor/config.js ckeditor/styles.js
         ckeditor/skins/moono/editor.css ckeditor/lang/en.js
-        rad_social/rad_screen.css lib/rad_social/captcha.js lib/rad_social/rad_email.js lib/rad_social/rad_widget.js)
+        rad_social/rad_screen.css rad_social/captcha.js rad_social/rad_widget.js)
     end
   end
 end
