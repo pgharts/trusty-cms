99547a558b2e5875c133311f0e5a67ca5a566809
Danielle Greaves <danigreaves@gmail.com>
2017-07-21 15:45:06 -0400

    BugzID: 4248 - Loading back in rad_email so the emails work again - whoops!

---
+++ app/assets/javascripts/rad_social/rad_email.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require 'rad_social/jquery.validate.min'
+//= require 'rad_social/rad_ajax_form'
+//= require 'rad_social/captcha'
+//= require 'rad_social/rad_email_validator'
+//= require 'rad_social/rad_email_form'
