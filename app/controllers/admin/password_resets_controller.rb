e8cf118be094e645d205cb4004ecaa11dc3ebcdf
Brittany Martin <brittany.jill.martin@gmail.com>
2017-09-15 08:01:44 -0400

    Register trusty-cms 3.1.1 and repair forgot password for Admin

--- app/controllers/admin/password_resets_controller.rb
+++ app/controllers/admin/password_resets_controller.rb
@@ -20,10 +20,17 @@ class Admin::PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    elsif @user.update_attributes(params[:user])
+    elsif @user.update_attributes(password_params)
       redirect_to welcome_url, :notice => "Password has been reset!"
     else
       render :edit
     end
   end
+
+private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
 end
