fe44f35b5915c88370383d17ee9c1e4f4ab3b7b8
danigirl329 <greaves@trustarts.orgl>
2021-07-28 14:13:18 -0400

    Change trusty-cms manifest to engine-name-manifest

--- app/assets/config/trusty-cms/manifest.js
+++
@@ -1,7 +0,0 @@
-//= link_tree ../../images
-//= link_directory ../../javascripts .js
-//= link_directory ../../stylesheets .css
-//= link admin/assets.css
-//= link admin/assets_admin.js
-//= link admin/assets_admin.js
-//= link admin/custom_file_upload.js
