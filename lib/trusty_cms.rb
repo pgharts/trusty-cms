61c88ce4c719c667ee2dc94a8527923745d5ef89
danigirl329 <greaves@trustarts.orgl>
2021-10-04 15:31:11 -0400

    Update version to 5.0.0

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = '4.3.4'.freeze
+    VERSION = '5.0.0'.freeze
   end
 end
