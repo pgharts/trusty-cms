9328812cc856aa6ca3e1e9fd6257c1ea2ef5947e
danigirl329 <danigreaves@gmail.com>
2021-10-05 11:10:36 -0400

    Rubo on deprecation file

--- lib/trusty_cms/deprecation.rb
+++ lib/trusty_cms/deprecation.rb
@@ -2,13 +2,12 @@ require 'active_support'
 
 class AssetType
   def paperclip_processors
-    ActiveSupport::Deprecation.warn("Paperclip processors will be deprecated soon in favor of ActiveStorage.")
+    ActiveSupport::Deprecation.warn('Paperclip processors will be deprecated soon in favor of ActiveStorage.')
 
     active_storage_styles
   end
 
   def paperclip_styles
-    ActiveSupport::Deprecation.warn("Paperclip styles will be deprecated soon in favor of ActiveStorage.")
+    ActiveSupport::Deprecation.warn('Paperclip styles will be deprecated soon in favor of ActiveStorage.')
   end
-
 end
