632542c88f7fbbd9b81866173ed59d74c1997d81
Donavin <donavin@trustarts.org>
2021-10-25 13:38:47 -0400

    remove if_field

--- app/models/standard_tags.rb
+++ app/models/standard_tags.rb
@@ -1079,28 +1079,6 @@ module StandardTags
   end
 
   desc %(
-    Renders the contained elements if the field given in the @name@ attribute
-    exists. The tag also takes an optional @equals@ or @matches@ attribute;
-    these will expand the tag if the field's content equals or matches the
-    given string or regex.
-
-    *Usage:*
-
-    <pre><code><r:if_field name="author" [equals|matches="John"] [ignore_case="true|false"]>...</r:if_field></code></pre>
-  )
-  tag 'if_field' do |tag|
-    required_attr(tag, 'name')
-    field = tag.locals.page.field(tag.attr['name'])
-    return '' if field.nil?
-
-    tag.expand if if tag.attr['equals'] && (tag.attr['ignore_case'] == 'false') then field.content == tag.attr['equals']
-                  elsif tag.attr['equals'] then field.content.downcase == tag.attr['equals'].downcase
-                  elsif tag.attr['matches'] then field.content =~ build_regexp_for(tag, 'matches')
-                  else field
-    end
-  end
-
-  desc %(
     The opposite of @if_field@. Renders the contained elements unless the field
     given in the @name@ attribute exists. The tag also takes an optional
     @equals@ or @matches@ attribute; these will expand the tag unless the
