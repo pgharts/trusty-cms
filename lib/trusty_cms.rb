a2a6853024165d88a6c393220fa71f11b9a6580e
danigirl329 <danigreaves@gmail.com>
2022-03-03 13:02:02 -0500

    Update gem version and bundle

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = '5.0.6'.freeze
+    VERSION = '5.0.7'.freeze
   end
 end
