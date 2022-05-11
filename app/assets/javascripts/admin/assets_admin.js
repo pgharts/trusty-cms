346f8c55319dab8717833fb59f35d6e02e59a09d
danigirl329 <danigreaves@gmail.com>
2022-05-11 13:28:09 -0400

    Remove static jquery files and replace with yarn dependencies

--- app/assets/javascripts/admin/assets_admin.js
+++ app/assets/javascripts/admin/assets_admin.js
@@ -10,6 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require 'admin/jquery.iframe-transport'
-//= require 'admin/jquery.fileupload'
+//= require 'blueimp-file-upload/js/jquery.fileupload.js'
 //= require 'admin/assets'
--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,21 +1,21 @@
-CKEDITOR.editorConfig = function( config ) {
-  config.allowedContent = true;
-  config.removeFormatTags = "";
-  config.protectedSource.push(/<r:([\S]+).*<\/r:\1>/g);
-  config.protectedSource.push(/<r:[^>/]*\/>/g);
-  //let paste from word be available
-  // config.forcePasteAsPlainText = false;
-  // if you want to remove clipboard, you have to remove all of these:
-  // clipboard, pastetext, pastefromword
-  config.removePlugins = "save, newpage, preview, print, templates, forms, flash, smiley, language, pagebreak, iframe, bidi";
+CKEDITOR.editorConfig = function (config) {
+    config.allowedContent = true;
+    config.removeFormatTags = "";
+    config.protectedSource.push(/<r:([\S]+).*<\/r:\1>/g);
+    config.protectedSource.push(/<r:[^>/]*\/>/g);
+    //let paste from word be available
+    // config.forcePasteAsPlainText = false;
+    // if you want to remove clipboard, you have to remove all of these:
+    // clipboard, pastetext, pastefromword
+    config.removePlugins = "save, newpage, preview, print, templates, forms, flash, smiley, language, pagebreak, iframe, bidi";
 
-  var startupMode = $.cookie('ckeditor.startupMode');
-  if (startupMode == 'source' || startupMode == 'wysiwyg' ) {
-    config.startupMode = startupMode;
-  }
+    var startupMode = Cookies.get('ckeditor.startupMode');
+    if (startupMode == 'source' || startupMode == 'wysiwyg') {
+        config.startupMode = startupMode;
+    }
 
-  this.on('mode', function(){
-    $.cookie('ckeditor.startupMode', this.mode);
-  })
+    this.on('mode', function () {
+        Cookies.set('ckeditor.startupMode', this.mode);
+    })
 
 };
