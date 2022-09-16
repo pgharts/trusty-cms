427e53fee425f02a87da5c37d7a8528ee15c5b6d
Donavin <donavin@trustarts.org>
2022-09-16 12:33:42 -0400

    version bump

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = '5.3.6'.freeze
+    VERSION = '5.3.8'.freeze
   end
 end
