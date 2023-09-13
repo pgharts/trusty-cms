d54f2e72599c84994ed3502cdda8991a357d1d12
Donavin <donavin@trustarts.org>
2023-09-13 12:27:06 -0400

    Bump v. 5.5.9, dependas, and cc rubo channel

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,6 +2,6 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = '5.5.8'.freeze
+    VERSION = '5.5.9'.freeze
   end
 end
