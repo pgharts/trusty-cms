d6178e581313855e39b38c4f6e26bf480cb2e3ce
danigirl329 <danigreaves@gmail.com>
2023-10-19 16:22:45 -0400

    Upgrade version to 6.0.0

--- lib/trusty_cms.rb
+++ lib/trusty_cms.rb
@@ -2,7 +2,7 @@ TRUSTY_CMS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unle
 
 unless defined? TrustyCms::VERSION
   module TrustyCms
-    VERSION = '5.6.0'.freeze
+    VERSION = '6.0.0'.freeze
   end
 end
 
