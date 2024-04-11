c3108466e1ce8ecd559971004ff6e23fec90dc4e
danigirl329 <danigreaves@gmail.com>
2024-04-11 12:06:36 -0400

    refactor alias_method

--- vendor/extensions/multi-site-extension/lib/multi_site/site_controller_extensions.rb
+++ vendor/extensions/multi-site-extension/lib/multi_site/site_controller_extensions.rb
@@ -1,26 +1,27 @@
 module MultiSite::SiteControllerExtensions
-  def self.included(base)
-    base.class_eval do
-      before_action :set_site
-      alias_method :process_page_without_home_path, :process_page
-      alias_method :process_page, :process_page_with_home_path
-    end
-  end
 
   # If it's a file not found page and the path doesn't include
   # the site's homepage path, try redirecting to include it.
-  def process_page_with_home_path(page)
-    homepage = Page.current_site.homepage
-    if page.is_a?(FileNotFoundPage) && !params[:url].include?(homepage.slug)
-      if homepage.slug != "/"
-        false if redirect_to "/#{homepage.slug}/#{params[:url]}"
+  module ProcessPageWithHomePath
+    def process_page(page)
+      homepage = Page.current_site.homepage
+      if page.is_a?(FileNotFoundPage) && !params[:url].include?(homepage.slug)
+        if homepage.slug != "/"
+          false if redirect_to "/#{homepage.slug}/#{params[:url]}"
+        else
+          super(page)
+        end
       else
-        process_page_without_home_path(page)
+        super(page)
       end
-    else
-      process_page_without_home_path(page)
     end
+  end
 
+  def self.included(base)
+    base.class_eval do
+      before_action :set_site
+      prepend ProcessPageWithHomePath
+    end
   end
 
   def set_site
