091b864fa4f896f4cc95c06f72997d898091cd67
danigirl329 <danigreaves@gmail.com>
2024-11-15 13:21:58 -0500

    initial swap from designer to editor

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -2,7 +2,7 @@ module Admin::UsersHelper
   def roles(user)
     roles = []
     roles << I18n.t('admin') if user.admin?
-    roles << I18n.t('designer') if user.designer?
+    roles << I18n.t('editor') if user.editor?
     roles.join(', ')
   end
 end
