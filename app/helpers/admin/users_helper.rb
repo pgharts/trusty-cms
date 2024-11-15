d7fda3d772123b41615265b7e62d1e702e8a87cf
danigirl329 <danigreaves@gmail.com>
2024-11-15 13:58:45 -0500

    add content editor to role list

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -3,6 +3,7 @@ module Admin::UsersHelper
     roles = []
     roles << I18n.t('admin') if user.admin?
     roles << I18n.t('editor') if user.editor?
+    roles << I18n.t('content_editor') if user.content_editor?
     roles.join(', ')
   end
 end
