e4c4a8663572c9cd059368202b3713110ea410c1
danigirl329 <danigreaves@gmail.com>
2024-11-15 13:56:43 -0500

    redo designer

--- app/controllers/admin/snippets_controller.rb
+++ app/controllers/admin/snippets_controller.rb
@@ -1,7 +1,7 @@
 class Admin::SnippetsController < Admin::ResourceController
   paginate_models
   only_allow_access_to :index, :show, :new, :create, :edit, :update, :remove, :destroy,
-                       when: %i[editor admin],
+                       when: %i[designer admin],
                        denied_url: { controller: 'admin/pages', action: 'index' },
                        denied_message: 'You must have at least editor privileges to perform this action.'
 end
