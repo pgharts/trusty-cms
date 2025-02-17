567a7d8c7e38f660bc7cbf63ad467b7332b899b5
Jared Blumer <blumer@trustarts.org>
2025-02-17 12:52:54 -0500

    Update gem version to 7.0.19

--- config/initializers/trusty_cms_config.rb
+++ config/initializers/trusty_cms_config.rb
@@ -88,7 +88,4 @@ Rails.application.reloader.to_prepare do
   if TrustyCms.config_definitions['defaults.snippet.filter'].nil?
     TrustyCms.config.define 'defaults.snippet.filter', select_from: lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, allow_blank: true
   end
-
-  Admin::LayoutsController.send :helper, MultiSite::SiteChooserHelper
-  Admin::SnippetsController.send :helper, MultiSite::SiteChooserHelper
 end
