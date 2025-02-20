74f9b0678861b4e5793ebe7f5e2b330955c76555
Jared Blumer <blumer@trustarts.org>
2025-02-20 15:29:49 -0500

    Refactor to reduce complexity and fix CodeClimate issues

--- app/helpers/admin/url_helper.rb
+++ app/helpers/admin/url_helper.rb
@@ -1,47 +1,44 @@
 module Admin::UrlHelper
   require 'uri'
 
-  def extract_base_url(url)
-    uri = URI.parse(url)
-    return "#{uri.scheme}://#{uri.host}:#{uri.port}" if uri.host&.include?('localhost')
-
-    "#{uri.scheme}://#{uri.host}"
-  end
-
-  def generate_page_url(url, page)
-    return unless page&.respond_to?(:slug)
-
-    base_url = extract_base_url(url)
-
-    if page.class.name == "Page"
-      return "#{base_url}#{page.path}"
+  def build_url(base_url, page)
+    if page.class.name == 'Page'
+      "#{base_url}#{page.path}"
     else
       path = lookup_page_path(page)
       path ? "#{base_url}/#{path}/#{page.slug}" : nil
     end
   end
 
+  def extract_base_url(url)
+    uri = URI.parse(url)
+    host = uri.host
+    scheme = uri.scheme
+    return "#{scheme}://#{host}:#{uri.port}" if host&.include?('localhost')
+
+    "#{scheme}://#{host}"
+  end
+
+  def generate_page_url(url, page)
+    base_url = extract_base_url(url)
+    build_url(base_url, page)
+  end
+
   def lookup_page_path(page)
     PAGE_TYPES[page.class.name.to_sym]
   end
 
-  private
-
   PAGE_TYPES = {
-    BlogPage: "blog",
-    DonationPage: "donate",
-    ExhibitionPage: "exhibit",
-    FlexPackagePage: "flex_package",
-    MembershipPage: "membership",
-    MultiViewPage: "multiview",
-    NonTicketedEventPage: "event",
-    PackagePage: "package",
+    BlogPage: 'blog',
+    DonationPage: 'donate',
+    ExhibitionPage: 'exhibit',
+    FlexPackagePage: 'flex_package',
+    MembershipPage: 'membership',
+    MultiViewPage: 'multiview',
+    NonTicketedEventPage: 'event',
     PersonPage: 'biography',
-    ProductionPage: "production",
-    RegistrationEventPage: "registration",
-    RenewalPage: "package/renewal",
-    SimpleDonationPage: "donationfeed",
-    SuperPackagePage: "super_package",
-    TimedEntryPage: "timed-entry",
+    ProductionPage: 'production',
+    RegistrationEventPage: 'registration',
+    SimpleDonationPage: 'donationfeed',
   }.freeze
 end
