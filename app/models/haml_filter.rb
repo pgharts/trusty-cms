a92936b3a03977ccf33aeede321814205cb36e3d
Jared Blumer <jaredblumer@protonmail.com>
2025-07-22 15:51:28 -0400

    Revert change to regular expression

--- app/models/haml_filter.rb
+++ app/models/haml_filter.rb
@@ -1,6 +1,6 @@
 class HamlFilter < TextFilter
   def filter(text)
     html = Haml::Template.new { text }.render
-    html.gsub(/&lt;(\/)?r:(.+?)\s*(\/?\\?)&gt;/m, '<\1r:\2\3>')
+    html.gsub(/&lt;(\/)?r:(.+?)\s*(\/?\\?)&gt;/m, '<\\1r:\\2\\3>')
   end
 end
