f1c2a39bf6f6365e483bca3883e0c64600697d9b
Danielle Greaves <danigreaves@gmail.com>
2025-10-03 09:56:34 -0400

    dry up asset controller

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -31,56 +31,39 @@ class Admin::AssetsController < Admin::ResourceController
 
   def uploader
     @page_attachments = []
-    uploaded_asset = compress(asset_params[:upload]) if $kraken.api_key.present? && COMPRESS_FILE_TYPE.include?(asset_params[:upload].content_type) && can_compress
+    result = process_uploaded_asset(asset_params[:upload])
 
-    unless uploaded_asset.present?
-      flash[:error] = 'No file uploaded.'
-      render json: { error: 'No file uploaded.' }, status: :unprocessable_entity
-      return
-    end
-
-    unless APPROVED_CONTENT_TYPES.include?(uploaded_asset.content_type)
-      flash[:error] = 'Unsupported file type.'
-      render json: { error: 'Unsupported file type.' }, status: :unsupported_media_type
-      return
-    end
-
-    @asset = Asset.create(asset: uploaded_asset, caption: '')
-
-    if @asset.valid?
-      set_owner_or_editor
+    if result[:asset]
+      @asset = result[:asset]
       if params[:for_attachment]
         @page = Page.find_by_id(params[:page_id]) || Page.new
-        @page_attachments << @page_attachment = @asset.page_attachments.build(page: @page)
+        @page_attachments << (@page_attachment = @asset.page_attachments.build(page: @page))
       end
+
       render json: { url: @asset.asset.url }
     else
-      error = @asset.errors.first.message
-      flash[:error] = error
-      render json: { error: error }, status: :unprocessable_entity
+      flash[result.fetch(:flash_type, :error)] = result[:error]
+      render json: { error: result[:error] }, status: result.fetch(:status, :unprocessable_entity)
     end
   end
 
   def create
     @assets = []
     @page_attachments = []
-    asset_params["asset"]["asset"].reject(&:blank?).each do |uploaded_asset|
-      if uploaded_asset.content_type == 'application/octet-stream'
-        flash[:notice] = 'Please only upload assets that have a valid extension in the name.'
-      else
-        uploaded_asset = compress(uploaded_asset) if $kraken.api_key.present? && COMPRESS_FILE_TYPE.include?(uploaded_asset.content_type) && can_compress
-        @asset = Asset.create(asset: uploaded_asset, caption: '')
-        if @asset.valid?
-          set_owner_or_editor
-          if params[:for_attachment]
-            @page = Page.find_by_id(params[:page_id]) || Page.new
-            @page_attachments << @page_attachment = @asset.page_attachments.build(page: @page)
-          end
-          @assets << @asset
-        else
-          error = @asset.errors.first.message
-          flash[:error] = error
+    uploads = Array(asset_params.dig('asset', 'asset')).reject(&:blank?)
+
+    uploads.each do |uploaded_asset|
+      result = process_uploaded_asset(uploaded_asset)
+
+      if result[:asset]
+        @asset = result[:asset]
+        if params[:for_attachment]
+          @page = Page.find_by_id(params[:page_id]) || Page.new
+          @page_attachments << (@page_attachment = @asset.page_attachments.build(page: @page))
         end
+        @assets << @asset
+      else
+        flash[result.fetch(:flash_type, :error)] = result[:error]
       end
     end
 
@@ -104,8 +87,42 @@ class Admin::AssetsController < Admin::ResourceController
 
   private
 
-  def can_compress
+  def process_uploaded_asset(uploaded_asset)
+    return failure_response('No file uploaded.', :unprocessable_entity, :error) unless uploaded_asset.present?
+
+    if uploaded_asset.content_type == 'application/octet-stream'
+      return failure_response('Please only upload assets that have a valid extension in the name.', :unprocessable_entity, :notice)
+    end
+
+    unless APPROVED_CONTENT_TYPES.include?(uploaded_asset.content_type)
+      return failure_response('Unsupported file type.', :unsupported_media_type, :error)
+    end
+
+    processed_upload = maybe_compress(uploaded_asset)
+    asset = Asset.create(asset: processed_upload, caption: '')
+
+    if asset.valid?
+      set_owner_or_editor(asset)
+      { asset: asset }
+    else
+      failure_response(asset.errors.full_messages.first, :unprocessable_entity, :error)
+    end
+  end
+
+  def maybe_compress(uploaded_asset)
+    should_compress?(uploaded_asset.content_type) ? compress(uploaded_asset) : uploaded_asset
+  end
+
+  def should_compress?(content_type)
+    $kraken.api_key.present? && COMPRESS_FILE_TYPE.include?(content_type) && compression_enabled?
+  end
+
+  def compression_enabled?
     current_site.try(:compress).nil? ? true : current_site.compress
+  end
+
+  def failure_response(message, status, flash_type)
+    { error: message, status: status, flash_type: flash_type }
   end
 
   def compress(uploaded_asset)
@@ -116,10 +133,12 @@ class Admin::AssetsController < Admin::ResourceController
     uploaded_asset
   end
 
-  def set_owner_or_editor
-    @asset.created_by_id = current_user.id
-    @asset.updated_by_id = current_user.id
-    @asset.save! if @asset.id.present?
+  def set_owner_or_editor(asset = @asset)
+    return unless asset
+
+    asset.created_by_id = current_user.id
+    asset.updated_by_id = current_user.id
+    asset.save! if asset.id.present?
   end
 
   def asset_params
