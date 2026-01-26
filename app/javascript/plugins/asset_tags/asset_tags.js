4839ae279c1bbedb12611ae7471474f28198a7eb
Danielle Greaves <danigreaves@gmail.com>
2026-01-26 12:10:53 -0500

    fix the editor loading, refactor asset builder

--- app/javascript/plugins/asset_tags/asset_tags.js
+++
@@ -1,8 +0,0 @@
-import AssetTagBuilder from './asset_tag_builder';
-import { Plugin } from 'ckeditor5';
-
-export default class AssetTags extends Plugin {
-  static get requires() {
-    return [ AssetTagBuilder ];
-  }
-}
